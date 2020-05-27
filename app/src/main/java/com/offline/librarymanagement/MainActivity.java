372b6e6aead3cd8c006cf84cbb782d10f25a0f40
xoriant99@iiht.tech <xoriant99@iiht.tech>
2020-05-27 13:03:31 +0530

    Initial commit

---
+++ app/src/main/java/com/offline/librarymanagement/MainActivity.java
@@ -0,0 +1,19 @@
+package com.offline.librarymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //start the main screen
+        startActivity(new Intent(getApplicationContext(),MainScreenActivity.class));
+
+    }
+}
