e62e5e811367425a30a975fc1f31167bfdd19d44
Shuvendu Dhal <shuvenduoffline@gmail.com>
2020-05-29 22:03:48 +0530

    Loaded search annimation..

--- app/src/main/java/com/offline/librarymanagement/FirebaseReprository/FireBaseRepro.java
+++ app/src/main/java/com/offline/librarymanagement/FirebaseReprository/FireBaseRepro.java
@@ -2,6 +2,7 @@ package com.offline.librarymanagement.FirebaseReprository;
 
 import android.util.Log;
 import android.util.Size;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -15,10 +16,11 @@ import com.google.firebase.firestore.QuerySnapshot;
 import com.offline.librarymanagement.Interfaces.onBookFetch;
 import com.offline.librarymanagement.model.Book;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FireBaseRepro {
-    String TAG = "FirebaseRepro";
+    final String TAG = "FirebaseRepro";
     FirebaseFirestore fb = FirebaseFirestore.getInstance();
     CollectionReference booksRef = fb.collection("BooksCollection");
     onBookFetch book_listner;
@@ -27,14 +29,18 @@ public class FireBaseRepro {
         this.book_listner = listner;
     }
 
-    public void getBookslist(String search){
+    public void getBookslist(final String search){
 
-        if (search == null || search == "") {
+
+        // logd
+        Log.d(TAG, "getBookslist: "+search);
+        if (search == null || search.equals("")) {
             booksRef.orderBy("createdon").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                 @Override
                 public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                     List<Book> booklist = queryDocumentSnapshots.toObjects(Book.class);
                     book_listner.onComplete(booklist);
+                    Log.d(TAG, "onSuccess: ");
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
@@ -43,11 +49,12 @@ public class FireBaseRepro {
                 }
             });
         } else {
-            booksRef.whereArrayContains("keywords", search).orderBy("createdon").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            booksRef.whereArrayContains("keywords", search).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                 @Override
                 public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                     List<Book> booklist = queryDocumentSnapshots.toObjects(Book.class);
                     book_listner.onComplete(booklist);
+                    Log.d(TAG, "onSuccess: "+search);
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
--- app/src/main/java/com/offline/librarymanagement/ui/home/HomeFragment.java
+++ app/src/main/java/com/offline/librarymanagement/ui/home/HomeFragment.java
@@ -1,10 +1,20 @@
 package com.offline.librarymanagement.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +25,7 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.offline.librarymanagement.MyAdapters.BooksAdapter;
 import com.offline.librarymanagement.R;
 import com.offline.librarymanagement.model.Book;
@@ -27,12 +38,14 @@ public class HomeFragment extends Fragment {
     private EditText searchInput;
     private RecyclerView booksRv;
     private BooksAdapter adapter;
+    private ImageView loadingImg;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         searchInput = root.findViewById(R.id.search_input);
         booksRv = root.findViewById(R.id.books_rv);
+        loadingImg = root.findViewById(R.id.loading_image);
         return root;
     }
 
@@ -41,19 +54,59 @@ public class HomeFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
 
+
         //view model
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-        homeViewModel.getBooks("").observe(getViewLifecycleOwner(), new Observer<List<Book>>() {
+        queryBook("");
+
+        //Hide the soft key on page open
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        //start the animation
+        showLoadingGif();
+
+        //on ime button press sone
+        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onChanged(List<Book> books) {
-                updateBookRv(books);
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    showLoadingGif();
+                   queryBook(searchInput.getText().toString().trim());
+                }
+                return false;
             }
         });
 
     }
 
+    public void queryBook(String search){
+        homeViewModel.getBooks(search).observe(getViewLifecycleOwner(), new Observer<List<Book>>() {
+            @Override
+            public void onChanged(List<Book> books) {
+                updateBookRv(books);
+            }
+        });
+    }
+
+    void showLoadingGif(){
+            booksRv.setVisibility(View.GONE);
+            loadingImg.setVisibility(View.VISIBLE);
+        Glide
+                .with( getActivity().getApplicationContext() )
+                .load(R.drawable.book_loading)
+                .into( loadingImg );
+    }
+
+
+
     private void updateBookRv(List<Book> books) {
-        if (books == null) return;
+        if (books == null || books.isEmpty()) return;
+        if( books.isEmpty()) {
+            showNoResultFound();
+            return;
+        }
+        booksRv.setVisibility(View.VISIBLE);
+        loadingImg.setVisibility(View.GONE);
         adapter = new BooksAdapter(books, getActivity().getApplicationContext());
         booksRv.setHasFixedSize(true);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
@@ -61,5 +114,9 @@ public class HomeFragment extends Fragment {
         booksRv.setItemAnimator(new DefaultItemAnimator());
         booksRv.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+    }
+
+    private void showNoResultFound() {
+
     }
 }
